280d214c72c343e67a7108af4a60ad28531aac9e
dlcdlc5055 <nicolae.m.mihaialexandru24@stud.rau.ro>
2026-07-10 17:31:24 +0300

    update

--- frontend/src/scripts/objects.ts
+++ frontend/src/scripts/objects.ts
@@ -43,11 +43,25 @@ class FishierMaterie{
   }
 }
 
+export class MaterieImgGroup {
+    title: string = "";
+    images: FishierMaterie[] = [];
+
+    addImage(image: FishierMaterie): void {
+        this.images.push(image);
+    }
+
+    removeImageAt(index: number): FishierMaterie | null {
+        if (index < 0 || index >= this.images.length) return null;
+        return this.images.splice(index, 1)[0];
+    }
+}
+
 export class Materie {
   name: string;
   quizs: Quiz[] = [];
   files: FishierMaterie[] = [];
-  imageGroups: ImageGroup[] = [];
+  imgs: MaterieImgGroup[] = [];
 
   constructor(name: string) {
     this.name = name;
@@ -383,36 +397,3 @@ export class StyleConfigList {
 
 
 export {FishierMaterie,Intrebare,Quiz,StudyGroup}
-
-
-export class Image {
-  path: string;
-  text: string;
-
-  constructor(path: string, text: string) {
-    this.path = path;
-    this.text = text;
-  }
-}
-
-export class ImageGroup {
-  title: string;
-  images: Image[];
-
-  constructor(title: string, images?: Image[]) {
-    this.title = title ?? '';
-    this.images = images || [];
-  }
-
-  addImage(image: Image): void {
-    this.images.push(image);
-  }
-
-  removeImageAt(index: number): Image | null {
-    if (index < 0 || index >= this.images.length) return null;
-    return this.images.splice(index, 1)[0];
-  }
-}
-
-
-
